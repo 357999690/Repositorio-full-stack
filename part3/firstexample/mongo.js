01f8d25ffc205dc0452ed260e75751b4627f97c4
unknown <luvaa30@gmail.com>
2024-02-23 13:05:02 -0300

    fisrt commit 4-a-a

--- part3/firstexample/mongo.js
+++ part3/firstexample/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if(process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,15 +14,15 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-    content:'HTML is easy',
-    important: true,
+  content:'HTML is easy',
+  important: true,
 })
 
 // note.save().then(result => {
@@ -31,9 +31,9 @@ const note = new Note({
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
+  result.forEach(note => {
+    console.log(note)
+  })
 
-    mongoose.connection.close()
+  mongoose.connection.close()
 })
