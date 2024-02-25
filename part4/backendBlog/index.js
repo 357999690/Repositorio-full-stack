5f7b3cd0ad2ceba45104a85d2960bab26d17082f
unknown <luvaa30@gmail.com>
2024-02-25 14:55:54 -0300

    segundo commint

--- part4/backendBlog/index.js
+++ part4/backendBlog/index.js
@@ -1,51 +1,50 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method: ', request.method)
-    console.log('Path: ', request.path)
-    console.log('Body: ', request.body)
-    console.log('---')
-    next()
-}
-
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
 
-app.post('/api/blogs', (request, response) => {
-    // const blog = request.body
-    // console.log(blog)
-    // response.json(blog)
-    const blog = new Blog(request.body)
+// const express = require('express')
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
-}
 
-app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3017
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+
+
+// mongoose.set('strictQuery', false)
+// mongoose.connect('mongodb+srv://lucaa20:LucasSimbron91@cluster0.7qsrmix.mongodb.net/blogs?retryWrites=true&w=majority')
+//     .then(() => {
+//         console.log('connected to MongoDb')
+//     })
+// const requestLogger = (request, response, next) => {
+//     console.log('Method: ', request.method)
+//     console.log('Path: ', request.path)
+//     console.log('Body: ', request.body)
+//     console.log('---')
+//     next()
+// }
+
+// app.use(cors())
+// app.use(express.json())
+// app.use(requestLogger)
+
+
+
+
+
+// const unknownEndpoint = (request, response) => {
+//     response.status(404).send({error: 'unknown endpoint'})
+// }
+
+// app.use(unknownEndpoint)
+
+// const PORT = process.env.PORT || 3017
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
